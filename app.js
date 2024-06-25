a26d6f19c83d66220d394778c59be20c6d5f2176
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-06-25 23:35:32 +0530

    add - adds multiple variable for each changaable content

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 import data from "./data.js";
-
 console.log(data);
+
+//Destinations
+const destinationImage = document.querySelector("#leftDestination img");
+const destinationHeader = document.querySelector("#destiheader");
+const destinationDescription = document.querySelector("#desticontent");
+
+// crew
+const crewImage = document.querySelector("#crewright img");
+const crewDesignation = document.querySelector("#crewContent h4");
+const crewName = document.querySelector("#crewContent h2");
+const crewDescription = document.querySelector("#crewContent p");
+
+// Technology
+const techName = document.querySelector("#content h1");
+const techDescription = document.querySelector("#content p");
+const techImage = document.querySelector("#content img");
+
+// destinationImage.attributes.src.value =data.destinations[2].images.png
+// console.log(destinationImage.attributes.src.value);
