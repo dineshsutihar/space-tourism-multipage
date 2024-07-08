1c0e2b6ab9d947e403f315c4a0a264338fe18097
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-07-08 22:17:23 +0530

    refactor: removed the unused codes

--- app.js
+++ app.js
@@ -19,3 +19,35 @@ const techImage = document.querySelector("#content img");
 
 // destinationImage.attributes.src.value =data.destinations[2].images.png
 // console.log(destinationImage.attributes.src.value);
+
+// observe weather the section appears
+
+// const crewSection = document.getElementById("crew");
+// function checkVisible(elm) {
+//   var rect = elm.getBoundingClientRect();
+//   var viewHeight = Math.max(
+//     document.documentElement.clientHeight,
+//     window.innerHeight
+//   );
+//   return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+// }
+
+// nav under line
+const navElement = document.querySelectorAll("header nav ul li");
+
+navElement.forEach((ele) => {
+  ele.addEventListener("click", (e) => {
+    e.target.classList.toggle("navActive");
+  });
+});
+
+// destination section click function
+const destElem = document.querySelectorAll("#rightDestination nav ul li");
+destElem.forEach((el) => {
+  console.log((el.target.value = "Dinesh"));
+  //   el.target.classList.contains("navActive") &&
+  //     el.target.classList.remove("navActive");
+  el.addEventListener("click", (event) => {
+    // console.log(event.target.classList.add("navActive"));
+  });
+});
